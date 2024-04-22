6c27f525d3e1b1ae417f641d6d0bf94d664ba08e
deepen-stha <deepenshrestha1@gmail.com>
2024-04-22 21:52:58 +0530

    removing NWP model from codebase

--- initialization.py
+++ initialization.py
@@ -1,17 +1,5 @@
-import pickle
-from transformers import pipeline
 import fasttext.util
 from ai4bharat.transliteration import XlitEngine
-
-def initialize_model():
-    """
-    Initialize the nepali_model global variable.
-    """
-    translator = pipeline("translation", model="facebook/nllb-200-distilled-600M", tokenizer="facebook/nllb-200-distilled-600M")
-
-    with open('model.pkl', 'rb') as file:
-        nepali_model = pickle.load(file)
-        return nepali_model, translator
 
 def initialize_fasttext_model():
     """
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 from flask import Flask, request
-from utils.predict_next_nepali_word import nextNepaliWord
-from utils.convert_language import translate_malyalam_to_nepali,translate_nepali_to_malyalam
+
 from initialization import *
 from flask_cors import CORS
 import sys
@@ -12,53 +11,8 @@ from utils.common import common_error_response,validate_transliteration_body
 app = Flask(__name__)
 CORS(app)
 
-# Load the model one time only at the beginning of the program
-nepali_model, translator = initialize_model()
-
-# load the fasttext model one time only
-# hindi_model, malyalam_model,nepali_fasttext_model  = initialize_fasttext_model()
-
 # loading the ai4bharat-transliteration model
 indic_to_hindi_xlite_engine, hindi_to_indic_xlite_engine = intializeXlitEngine()
-
-# adding route for predicting next word
-@app.route('/predict_next_word', methods=['POST'])
-def predict_next_word_api():
-    data = request.get_json()
-    context = data['context']
-    prediction_count = data['prediction-count']
-    language = data['language']
-    # if it is malyalam then 
-    if str.lower(language) == "malyalam":
-        nepali_word = translate_malyalam_to_nepali(context, translator)
-        prediction = nextNepaliWord(nepali_word, nepali_model, prediction_count)
-        malyalam_predicted_word = []
-        for pred in prediction:
-            malyalam_predicted_word.append(translate_nepali_to_malyalam(pred, translator))
-
-        json_response = json.dumps({"predictions": malyalam_predicted_word}, ensure_ascii=False)
-        return json_response
-    else:
-        prediction = nextNepaliWord(context, nepali_model, prediction_count)
-        json_response = json.dumps({"predictions": prediction}, ensure_ascii=False)
-        return json_response
-
-# # adding route for similar word
-# @app.route('/similar-word', methods=['POST'])
-# def perform_transliteration():
-#     data = request.get_json()
-#     context = data['context']
-#     prediction_count = data['prediction-count']
-#     source_script = data['source-script']
-#     source_sanscript = get_sanscript(source_script)
-
-#     fasttext_model = get_fasttext_model(source_sanscript,hindi_model,malyalam_model,nepali_fasttext_model)
-#     similar_words = get_similar_words(context, fasttext_model, prediction_count)
-#     predictions = []
-#     for score, word in similar_words:
-#         predictions.append(word)
-#     json_response = json.dumps({"predictions": predictions}, ensure_ascii=False)
-#     return json_response
 
 # adding route for transliterate
 @app.route('/transliterate', methods=['POST'])
@@ -85,20 +39,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "api":
         app.run(port=8080)
 
-    # elif len(sys.argv) > 1 and sys.argv[1] == "similar-word":
-    #     word = input("Enter a word which you want to transliterate: ")
-    #     prediction_count = int(input("Enter the number of predictions to generate: "))
-    #     source_script = input("Enter the source script:  ")
-    #     # target_script = input("Enter the target script")
-    #     source_sanscript = get_sanscript(source_script)
-    #     fasttext_model = get_fasttext_model(source_sanscript,hindi_model,malyalam_model,nepali_fasttext_model)
-    #     # transliterated_word = transliterate_word(word, source_script, target_script)
-    #     similar_words = get_similar_words(word, fasttext_model, 5)
-    #     predictions = []
-    #     for score, word in similar_words:
-    #         predictions.append(word)
-    #     print(predictions)
-
     elif len(sys.argv) > 1 and sys.argv[1] == "transliteration":
         word = input("Enter a word which you want to transliterate: ")
         prediction_count = int(input("Enter the number of predictions to generate: "))
@@ -114,19 +54,3 @@ if __name__ == "__main__":
         xlite_engine, lang_code, is_roman = get_transliteration_model(source_script,target_script, indic_to_hindi_xlite_engine, hindi_to_indic_xlite_engine)
         prediction = predict_transliteration(word, xlite_engine, lang_code, prediction_count,is_roman)
         print(prediction)
-
-    else:
-        # Interactively get input from the terminal
-        word = input("Enter a word to predict next words for: ")
-        prediction_count = int(input("Enter the number of predictions to generate: "))
-        language = input("Enter the language:  ")
-        if language == "nepali":
-            prediction = nextNepaliWord(word, nepali_model, prediction_count)
-        else:
-            nepali_word = translate_malyalam_to_nepali(word, translator)
-            nepali_predictions = nextNepaliWord(nepali_word, nepali_model, prediction_count)
-            predictions = []
-            for pred in nepali_predictions:
-                predictions.append(translate_nepali_to_malyalam(pred, translator))
-
-        print("Predictions:", predictions)
